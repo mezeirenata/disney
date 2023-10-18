b6aa38adb273f458afaa942682a728363e09b27c
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2023-10-18 10:01:03 +0200

    event fix, harcrendszer + loot kész

--- harcrendszer.py
+++ harcrendszer.py
@@ -1,7 +1,9 @@
 from random import randint
 import os
 
-def harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP, enemy_NAME):
+enemy_HP = 50
+
+def harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP,):
     player_ATK = player_ATK
     player_DEF = player_DEF
     player_DAM = player_DAM
@@ -10,7 +12,6 @@ def harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, en
     enemy_DEF = enemy_DEF
     enemy_DAM = enemy_DAM
     enemy_HP = enemy_HP
-    enemy_NAME = enemy_NAME
     player_true_ATK = randint(1, 100) + player_ATK
     enemy_true_ATK = randint(1, 100) + enemy_ATK
     if player_true_ATK > enemy_DEF:
@@ -31,14 +32,22 @@ def harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, en
     if win_player == "player":
         print("Talált a támadásod")
         enemy_HP -= player_DAM*randint(1, 4)
-        print(f"Ellenfeled életereje: {enemy_HP}")
+        if enemy_HP > 1:
+            print(f"Ellenfeled életereje: {enemy_HP}")
+        else:
+            print("Ellenség meghalt")
+            return
     elif win_player == "draw":
         print("Ez a köröd döntetlen.")
         print(f"Ellenfeled életereje: {enemy_HP}")
     elif win_player == "enemy":
         print("Az ellenfeled betalált")
         player_HP -= enemy_DAM*randint(1,4)
-        print(f"Az életerőd: {player_HP}")
+        if player_HP > 1:
+            print(f"Az életerőd: {player_HP}")
+        else:
+            print("Meghaltál")
+            return "halal"
     
     print("")
     print("Az ellenfeled köre következik")
@@ -47,26 +56,43 @@ def harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, en
         print("Talált a támadásod")
 
         enemy_HP -= player_DAM*randint(1, 4)
-        print(f"Ellenfeled életereje: {enemy_HP}")
+        if enemy_HP > 1:
+            print(f"Ellenfeled életereje: {enemy_HP}")
+        else:
+            print("Ellenség meghalt")
+            return "nyert"
     elif win_enemy == "draw":
         print("Ez a kör döntetlen.")
         print(f"Ellenfeled életereje: {enemy_HP}")
     elif win_enemy == "enemy":
         print("Az ellenfeled betalált")
         player_HP -= enemy_DAM*randint(1,4)
-        print(f"Az életerőd: {player_HP}")
+        if player_HP > 1:
+            print(f"Az életerőd: {player_HP}")
+        else:
+            print("Meghaltál")
+            return "halal"
 
     print("")
-    harc_menu(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP, enemy_NAME)
+    harc_menu(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP,)
 
-def harc_menu(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP, enemy_NAME):
+def harc_menu(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP,):
 
-    print("1 - Neki támadsz")
-    print("2 - Elmenekülsz")
-    valasz = int(input("Választásod: "))
-    if valasz == 1:
-        harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP, enemy_NAME)
-        os.system("cls")
+    if player_HP < 1:
+        end =  "halal"
+        return end
+    elif enemy_HP < 1:
+        end = "nyert"
+        return end
     else:
-        return
-harc_menu(40, 100, 4, 100, 30, 99, 2, 50, "Anyád")
+        print("1 - Neki támadsz")
+        print("2 - Elmenekülsz")
+        valasz = int(input("Választásod: "))
+        if valasz == 1:
+            os.system("cls")
+            end = harc(player_ATK, player_DEF, player_DAM, player_HP, enemy_ATK, enemy_DEF, enemy_DAM, enemy_HP,)
+        else:
+            end = "elfut"
+            return end
+        return end
+end = harc_menu(40, 100, 4, 100, 30, 99, 2, enemy_HP,)
