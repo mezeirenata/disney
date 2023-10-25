f14607d33c2254fc078492235371ed545be40e1a
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2023-10-25 09:23:29 +0200

    map_rajzol kivettem

--- map.py
+++ map.py
@@ -6,17 +6,8 @@ x_max = 3
 y_max = 10 
 player_x = 2
 player_y = 1
-def map_rajzol(player_x, player_y):
-    for o in range(1, y_max+1):
-        print()
-        for i in range(1, x_max+1):
-            if i == player_x and o == player_y:
-                print("[x]", end=" ")
-            else:
-                print("[ ]", end=" ")
 
-def map_igen(player_x, player_y):
-    map_rajzol(player_x, player_y)
+def map_helyzet(player_x, player_y):
     while True:
         asd = input("Mozog: ")
         if asd == "a" and player_x != 1:
@@ -28,5 +19,5 @@ def map_igen(player_x, player_y):
         elif asd == "s" and player_y != y_max:
             player_y = move.move_y(1, player_y)
         os.system("cls")
-        map_rajzol(player_x,player_y)
-map_igen(player_x, player_y)
+
+map_helyzet(player_x, player_y)
