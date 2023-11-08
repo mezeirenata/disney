3add01c2af3f586aaa4bda19db16c3b29afa7ca3
Renata Mezei <mezei.renata@students.jedlik.eu>
2023-11-08 10:17:38 +0100

    változtatások

--- move.py
+++
@@ -1,7 +0,0 @@
-def move_x(x_move,x):
-    x += x_move
-    return x
-
-def move_y(y_move,y):
-    y += y_move
-    return y
--- valtozok.py
+++
@@ -1,4 +0,0 @@
-valasztas = 7
-player_x = 2
-player_y = 1
-viragsziv = False
